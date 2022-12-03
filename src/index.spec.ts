3dd4a09c57d893f73987fde9e6d0b6a3af8ed3bd
Christopher Guilliams <chrisg07@vt.edu>
2022-12-03 10:04:08 -0600

    Day 3 Part A

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,40 @@
 // @ts-ignore see https://github.com/jest-community/jest-extended#setup
 import * as matchers from "jest-extended";
-import { Solution } from './index';
+import { Solution } from "./index";
 
 expect.extend(matchers);
 
-test('solve test input', () => {
-  const solution = new Solution().solve('./src/test-input.txt');
-  expect(solution).toBe(12);
+test("solve test input", () => {
+  const solution = new Solution().solve("./src/test-input.txt");
+  expect(solution).toBe(157);
 });
 
-test('solve input', () => {
-  const solution = new Solution().solve('./src/input.txt');
+test("getPriorityScore", () => {
+  const solution = new Solution();
+  let score = solution.getPriorityScore("a");
+  expect(score).toBe(1);
+  score = solution.getPriorityScore("A");
+  expect(score).toBe(27);
+  score = solution.getPriorityScore("z");
+  expect(score).toBe(26);
+  score = solution.getPriorityScore("Z");
+  expect(score).toBe(52);
+  score = solution.getPriorityScore("p");
+  expect(score).toBe(16);
+  score = solution.getPriorityScore("L");
+  expect(score).toBe(38);
+  score = solution.getPriorityScore("P");
+  expect(score).toBe(42);
+  score = solution.getPriorityScore("v");
+  expect(score).toBe(22);
+  score = solution.getPriorityScore("t");
+  expect(score).toBe(20);
+  score = solution.getPriorityScore("s");
+  expect(score).toBe(19);
+});
+
+test("solve input", () => {
+  const solution = new Solution().solve("./src/input.txt");
   expect(solution).toBeDefined();
-  console.log('The solution is: ', solution);
+  console.log("The solution is: ", solution);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const POINTS = {
   ROCK: 1,
@@ -6,15 +6,15 @@ const POINTS = {
   SCISSORS: 3,
   LOSS: 0,
   DRAW: 3,
-  WIN: 6
+  WIN: 6,
 };
 
 export class Solution {
   readInput(file: string): Array<string> {
-    let input = fs.readFileSync(file, 'utf8' , (err, data) => {
+    let input = fs.readFileSync(file, "utf8", (err, data) => {
       if (err) {
-        console.error(err)
-        return err
+        console.error(err);
+        return err;
       }
     });
 
@@ -25,11 +25,16 @@ export class Solution {
     const lines = this.readInput(file);
     let scores = new Array<number>();
     // print all lines
-    let currentCalories = 0;
+    let currentScore = 0;
     lines.forEach((line) => {
-      const game = line.split(' ');
-      if (game) {
-        scores.push(this.determineScore(game[0], game[1]));
+      const left = line.substring(0, line.length / 2);
+      const right = line.substring(line.length / 2);
+
+      for (let char of left) {
+        if (right.includes(char)) {
+          scores.push(this.getPriorityScore(char));
+          break;
+        }
       }
     });
 
@@ -38,58 +43,12 @@ export class Solution {
     return total;
   }
 
-  determineScore(player1: string, player2: string): number {
-    let score = 0;
-    switch (player2) {
-      case 'X':
-        score += 0;
-        switch (player1) {
-          case 'A':
-            score += 3;
-            break;
-          case 'B':
-            score += 1;
-            break;
-          case 'C':
-            score += 2;
-            break;
-          default:
-        }
-        break;
-      case 'Y':
-        score += 3;
-        switch (player1) {
-          case 'A':
-            score += 1;
-            break;
-          case 'B':
-            score += 2;
-            break;
-          case 'C':
-            score += 3;
-            break;
-          default:
-        }
-        break;
-      case 'Z':
-        score += 6;
-        switch (player1) {
-          case 'A':
-            score += 2;
-            break;
-          case 'B':
-            score += 3;
-            break;
-          case 'C':
-            score += 1;
-            break;
-          default:
-        }
-        break;
-      default:
+  getPriorityScore(char: string): number {
+    let score = char.codePointAt(0);
+    if (score > 96) {
+      return score - 96;
+    } else {
+      return score - 38;
     }
-
-    return score;
   }
 }
-
